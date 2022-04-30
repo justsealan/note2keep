1cb465d028c8ba9a137e6cfd3d1e139f3ceb7110
justsealan <justsealan@gmail.com>
2022-04-30 15:08:09 +0100

    Add scripts for github pages

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
     return (
         <footer className="footer">
         <p>
-            `Copyright &copy; {date}`
+            ` S.E.A. &copy; {date}`
         </p>
         </footer>)
 }
